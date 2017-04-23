72d126a8c751c6c19248e230d8c161f5db3b8bf6
lzc <2035941342@qq.com>
2017-04-24 02:10:08 +0800

    修正点击棋盘逻辑

--- client/Assets/board.cs
+++ client/Assets/board.cs
@@ -49,14 +49,16 @@ public class board : MonoBehaviour {
 		{ChessType.black_car, ChessType.black_horse, ChessType.black_elephant, ChessType.black_shi, ChessType.black_general, ChessType.black_shi, ChessType.black_elephant, ChessType.black_horse, ChessType.black_car},
 	};
 
+    private Dictionary<string, GameObject> _typeToChessObj = new Dictionary<string, GameObject>();
+
     // 轮到哪一方下子
     public GroupType CurrentGroup { get; set; }
 
     // 当前玩家属于哪一方
     public GroupType PlayerGroup{ get; set;}
 
-    // 玩家当前选中的棋子位置
-    private chessPos _currentSelectPos;
+    // 玩家当前选中的棋子
+    private GameObject _currentSelectChess;
 
 
     // 根据旗子类型获取阵营类型
@@ -97,6 +99,12 @@ public class board : MonoBehaviour {
         chessScript.Pos.wIndex = posW;
         chessScript.draw();
 
+        string key = string.Format("{0}_{1}_{2}", posH, posW, type);
+        if (_typeToChessObj.ContainsKey(key) == false)
+        {
+            _typeToChessObj.Add(key, chessObj);
+        }
+
     }
 
     // 绘制棋盘
@@ -118,16 +126,16 @@ public class board : MonoBehaviour {
     }
 
     // 处理用户对棋盘位置的点击事件
-    private void onClickPos(int posH, int posW)
+    private void onClickPos(int posH, int posW, Collider2D chessPosCollider)
     {
         // 判断是否轮到玩家操作
         if (PlayerGroup != CurrentGroup)
             return;
 
         ChessType selectChessType = _dataArr[posH, posW];
-
+        
         // 判断是选子 还是 落子
-        if (_currentSelectPos.hIndex == -1)
+        if (_currentSelectChess == null)
         {
             if (selectChessType == ChessType.empty)
                 return;
@@ -135,23 +143,54 @@ public class board : MonoBehaviour {
             if (getGroupType(selectChessType) != PlayerGroup)
                 return;
 
-            _currentSelectPos.hIndex = posH;
-            _currentSelectPos.wIndex = posW;
+            string key = string.Format("{0}_{1}_{2}", posH, posW, selectChessType);
+            if (_typeToChessObj.ContainsKey(key) == false)
+            {
+                Debug.Log("chessObj is not exist when selectChess!!!");
+                return;
+            }
+
+            _currentSelectChess = _typeToChessObj[key];
         }
         else
         {
-            if (selectChessType == ChessType.empty)
+            chess chessScript = _currentSelectChess.GetComponent<chess>();
+            if (chessScript == null)
             {
-                ChessType oldChessType = _dataArr[_currentSelectPos.hIndex, _currentSelectPos.wIndex];
-                _dataArr[_currentSelectPos.hIndex, _currentSelectPos.wIndex] = ChessType.empty;
-                _dataArr[posH, posW] = oldChessType;
+                Debug.Log("chessScript is null when luo zi");
+                return;
             }
 
-            _currentSelectPos.hIndex = -1;
-            _currentSelectPos.wIndex = -1;
+            // 落到己方的子上了
+            if (selectChessType != ChessType.empty && getGroupType(selectChessType) == PlayerGroup)
+                return;
 
-            // 绘制棋盘
-            drawBoard();
+            _dataArr[chessScript.Pos.hIndex, chessScript.Pos.wIndex] = ChessType.empty;
+            _dataArr[posH, posW] = chessScript.type;
+            string oldKey = string.Format("{0}_{1}_{2}", chessScript.Pos.hIndex, chessScript.Pos.wIndex, chessScript.type);
+            _typeToChessObj.Remove(oldKey);
+
+            chessScript.Pos.hIndex = posH;
+            chessScript.Pos.wIndex = posW;
+            string newKey = string.Format("{0}_{1}_{2}", chessScript.Pos.hIndex, chessScript.Pos.wIndex, chessScript.type);
+            _typeToChessObj.Add(newKey, _currentSelectChess);
+
+            // 移动棋子
+            _currentSelectChess.GetComponent<Transform>().position = chessPosCollider.GetComponent<Transform>().position;
+            _currentSelectChess = null;
+
+            // 如果是吃棋 删除吃掉的棋子对象
+            if (selectChessType != ChessType.empty)
+            {
+                string eattedKey = string.Format("{0}_{1}_{2}", posH, posW, selectChessType);
+                if (_typeToChessObj.ContainsKey(eattedKey))
+                {
+                    GameObject eattedChess = _typeToChessObj[eattedKey];
+                    _typeToChessObj.Remove(eattedKey);
+                    GameObject.Destroy(eattedChess);
+                }
+                
+            }
             
 
         }
@@ -169,9 +208,7 @@ public class board : MonoBehaviour {
         PlayerGroup = GroupType.red;
 
         // 玩家选中棋子置为null
-        _currentSelectPos = new chessPos();
-        _currentSelectPos.hIndex = -1;
-        _currentSelectPos.wIndex = -1;
+        _currentSelectChess = null;
 
 
 
@@ -193,7 +230,7 @@ public class board : MonoBehaviour {
                 Debug.Log(string.Format("pos is {0}, {1}", pos.hIndex, pos.wIndex));
 
 
-                onClickPos(pos.hIndex, pos.wIndex);
+                onClickPos(pos.hIndex, pos.wIndex, c);
 
             }
         }
